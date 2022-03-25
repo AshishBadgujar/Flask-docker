8e444730ca56e06326442748193693ca9a31da4b
Ashish <ashish@reapit.in>
2022-03-25 10:52:28 +0530

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
-FROM python:3.8-slim-buster
+FROM python:3.9
+WORKDIR /flask-docker
 
-WORKDIR /python-docker
-
-COPY requirements.txt requirements.txt
+COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 COPY . .
+ENV MY_ENV=AshishBadgujar
+EXPOSE 5000
 
-CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
+CMD ["flask","run","--host=0.0.0.0 ]
--- app.py
+++ app.py
@@ -1,13 +1,10 @@
+from crypt import methods
 from flask import Flask
 from dotenv import load_dotenv
 import os
-load_dotenv()
-# Flask constructor takes the name of
-# current module (__name__) as argument.
 app = Flask(__name__)
 
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
 def hello_world():
     return f'<p>Hello world, your ENV variable=<strong>{os.getenv("MY_ENV")}</strong></p>'
 
